44729d9b40abb9ce14f60b1525c2b62d615346c5
orange77 <2382731457@qq.com>
2022-12-05 13:24:30 +0800

    游戏库服务类

---
+++ gamelibrary_Server.py
@@ -0,0 +1,50 @@
+from game import games_list, games_id, games_name
+from gamelibrary import Library
+from gametogether import play_together
+
+userid=1 #根据用户id获取其对应的库
+user_library=Library(userid,games_list)
+print("***************************************")
+print("您的游戏库:(请输入游戏id进行游玩或其他操作)")
+user_library.listgames()
+print("**************************输入quit退出库**")
+
+while(1):
+    input1=input()
+    option_game=eval(input1)
+    if input1=="quit":
+        break
+    if option_game in games_id:
+        print("***************************************")
+        print(games_name[option_game-1])
+        print("1.本地游玩")
+        print("2.远程畅玩")
+        print("3.游戏社区")
+        print("***************************************")
+        input2=input()
+        option_model=eval(input2)
+        if input2 == "quit":
+            break
+        if option_model==1:
+            print("开始游玩" + games_name[option_game - 1])
+            print("读取游戏文件中。。。")
+            print("***************************************")
+            print("*                                     *")
+            print("*                 " + games_name[option_game - 1])
+            print("*                                     *")
+            print("***************************************")
+        elif option_model==2:
+            play_together()
+
+        elif option_model == 3:
+            print(games_name[option_game - 1] + "社区 ")
+            print("***************************************")
+            print("*创意工坊  评测  个人数据  最近新闻        *")
+            print("*                                     *")
+            print("*                                     *")
+            print("***************************************")
+
+        else:
+            print("请输入正确的指令")
+    else:
+        print("请输入正确的指令")
