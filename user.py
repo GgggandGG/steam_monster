d6b3e3e2a2bdb423f489276699e3ac566da118a6
hsppya <494592860@qq.com>
2022-12-07 21:31:28 +0800

    用户类
    买物品、卖物品、查看交易信息、展示仓库

---
+++ user.py
@@ -0,0 +1,28 @@
+from info import informations
+from warehouse import warehouse
+
+class user():
+    def __init__(self, uname):
+        self.WareHouse = warehouse(uname)
+        self.Information = informations(uname)
+
+    def showhouse(self):
+        self.WareHouse.show()
+
+    def buy(self, name, num, price, date, seller):
+        self.WareHouse.buyin(name, num)
+        self.Information.update(name, price, date, num, seller)
+
+    def sell(self, name, num, price, date, seller ):
+        self.WareHouse.sellout(name, num)
+        self.Information.update(name, price, date, num, seller)
+
+    def check(self, option):
+        if option=='1':
+            self.Information.printout1()
+        elif option=='2':
+            self.Information.printout2()
+        elif option == '3':
+            self.Information.printout3()
+        else:
+            print("请输入正确选项")
